d08aa025cb15778e0fe5cd84a3acd1ad98ac2145
DmitryKam <k8741460@gmail.com>
2020-10-19 22:17:10 +0300

    Updates

---
+++ precache-manifest.028a23ee6521579c77c40c25718e279e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666d6f6f674d0c885a96696f0d21d38e",
+    "url": "/1.2/index.html"
+  },
+  {
+    "revision": "978113e4a7545c2f13b7",
+    "url": "/1.2/static/css/main.5572a90c.chunk.css"
+  },
+  {
+    "revision": "c4f67b062a1485536046",
+    "url": "/1.2/static/js/2.8b5531f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1.2/static/js/2.8b5531f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978113e4a7545c2f13b7",
+    "url": "/1.2/static/js/main.3bf86866.chunk.js"
+  },
+  {
+    "revision": "7aa6c81f20b98c49da8a",
+    "url": "/1.2/static/js/runtime-main.92b0e5a0.js"
+  }
+]);
